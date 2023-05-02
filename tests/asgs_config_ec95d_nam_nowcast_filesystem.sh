72e194476d9469f537d17e8c11d6721d5a70e2dd
Jason Fleming <jason.fleming@seahorsecoastal.com>
2023-05-02 09:53:13 -0400

    Updated/added configs to test nowcasting from NAM data on local filesystem.

--- tests/asgs_config_ec95d_nam_nowcast_filesystem.sh
+++ tests/asgs_config_ec95d_nam_nowcast_filesystem.sh
@@ -36,7 +36,7 @@
 
 # set instancename based on mesh, forcing, and operator
 
-INSTANCENAME=EC95d_nam   # "op" nickname set in config/operator_defaults.sh
+INSTANCENAME=EC95d_nam_nowcast_filesystem   # "op" nickname set in config/operator_defaults.sh
 
 # Input files and templates
 
@@ -48,6 +48,8 @@ source $SCRIPTDIR/config/mesh_defaults.sh # get default settings for this mesh
 TIDEFAC=on            # tide factor recalc
 HINDCASTLENGTH=30.0   # length of initial hindcast, from cold (days)
 BACKGROUNDMET=on      # NAM download/forcing
+   BACKSITE=filesystem
+   BACKDIR=~/Campaigns/Development/2023/test-nam-nowcast-with-files/nam
 TROPICALCYCLONE=off   # tropical cyclone forcing
  STORM=05             # storm number, e.g. 05=ernesto in 2006
  YEAR=2019            # year of the storm
@@ -77,7 +79,7 @@ LASTSUBDIR=null
 # Scenario package 
 
 #PERCENT=default
-SCENARIOPACKAGESIZE=2 # number of scenarios
+SCENARIOPACKAGESIZE=2 # number of forward looking scenarios
 case $si in
 -2) 
    ENSTORM=hindcast
@@ -100,4 +102,3 @@ esac
 
 PREPPEDARCHIVE=prepped_${GRIDNAME}_${INSTANCENAME}_${NCPU}.tar.gz
 HINDCASTARCHIVE=prepped_${GRIDNAME}_hc_${INSTANCENAME}_${NCPU}.tar.gz
-
