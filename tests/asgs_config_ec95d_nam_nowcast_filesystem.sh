42d3d077d49e08cc54342ed623298580542aca9d
Jason Fleming <jason.fleming@seahorsecoastal.com>
2023-05-10 10:52:30 -0400

    Updated nam nowcast from filesystem for ian al092022.

--- tests/asgs_config_ec95d_nam_nowcast_filesystem.sh
+++ tests/asgs_config_ec95d_nam_nowcast_filesystem.sh
@@ -49,7 +49,8 @@ TIDEFAC=on            # tide factor recalc
 HINDCASTLENGTH=30.0   # length of initial hindcast, from cold (days)
 BACKGROUNDMET=on      # NAM download/forcing
    BACKSITE=filesystem
-   BACKDIR=~/Campaigns/Development/2023/test-nam-nowcast-with-files/nam
+   #BACKDIR=~/Campaigns/Development/2023/test-nam-nowcast-with-files/nam
+   BACKDIR=/mnt/nas-storage/Data/nam-al092022-ian
 TROPICALCYCLONE=off   # tropical cyclone forcing
  STORM=05             # storm number, e.g. 05=ernesto in 2006
  YEAR=2019            # year of the storm
@@ -72,14 +73,15 @@ POSTPROCESS=( includeWind10m.sh )
 
 # Initial state (overridden by STATEFILE after the first cycle)
 
-COLDSTARTDATE=2023033000
+#COLDSTARTDATE=2023033000
+COLDSTARTDATE=2022073000   # nam-al092022-ian
 HOTORCOLD=coldstart        # "hotstart" or "coldstart"
 LASTSUBDIR=null
 
 # Scenario package 
 
 #PERCENT=default
-SCENARIOPACKAGESIZE=2 # number of forward looking scenarios
+SCENARIOPACKAGESIZE=0 # number of forward looking scenarios
 case $si in
 -2) 
    ENSTORM=hindcast
