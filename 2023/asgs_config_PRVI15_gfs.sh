2def095d5fe5b92a4dfbb86afd714eee222e7705
Jason Fleming <jason.fleming@seahorsecoastal.com>
2023-06-22 12:53:14 -0500

    Reduced gfs forecast length to 3.5 days

--- 2023/asgs_config_PRVI15_gfs.sh
+++ 2023/asgs_config_PRVI15_gfs.sh
@@ -48,6 +48,7 @@ source $SCRIPTDIR/config/mesh_defaults.sh
 TIDEFAC=on            # tide factor recalc
 HINDCASTLENGTH=30.0   # length of initial hindcast, from cold (days)
 BACKGROUNDMET=GFS     # NAM download/forcing
+   GFSFORECASTLENGTH=84
 FORECASTCYCLE="00,06,12,18"
 TROPICALCYCLONE=off   # tropical cyclone forcing
 STORM=09              # storm number, e.g. 05=ernesto in 2006
