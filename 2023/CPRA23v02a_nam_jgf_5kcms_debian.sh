29bb51d1f12ec22c91555920b28e22d0a74a00df
Jason Fleming <jason.fleming@seahorsecoastal.com>
2023-10-24 13:51:09 -0400

    Updated Shinnecock test config to avoid unhcat.exe

--- 2023/CPRA23v02a_nam_jgf_5kcms_debian.sh
+++ 2023/CPRA23v02a_nam_jgf_5kcms_debian.sh
@@ -24,68 +24,73 @@
 # You should have received a copy of the GNU General Public License along with
 # the ASGS.  If not, see <http://www.gnu.org/licenses/>.
 #-------------------------------------------------------------------
+# The defaults for parameters that can be reset in this config file
+# are preset in the following scripts:
+# {SCRIPTDIR/platforms.sh               # also contains Operator-specific info
+# {SCRIPTDIR/config/config_defaults.sh
+# {SCRIPTDIR/config/mesh_defaults.sh
+# {SCRIPTDIR/config/forcing_defaults.sh
+# {SCRIPTDIR/config/io_defaults.sh
+# {SCRIPTDIR/config/operator_defaults.sh
+#-------------------------------------------------------------------
 
 # Fundamental
 
-INSTANCENAME=Shinnecock-gfs   # "name" of this ASGS process
+INSTANCENAME=CPRA23v02a_nam_jgf_5kcms  # "name" of this ASGS process
 
 # Input files and templates
 
-GRIDNAME=Shinnecock
+GRIDNAME=CPRA23v02a
 source $SCRIPTDIR/config/mesh_defaults.sh
+HOTSTARTFORMAT=netcdf3
 
-# Initial condition
+# Physical forcing (defaults set in config/forcing_defaults)
 
-COLDSTARTDATE=2023100500  # calendar year month day hour YYYYMMDDHH24
-HOTORCOLD=coldstart       # "hotstart" or "coldstart"
-LASTSUBDIR=null           # path to previous execution (if HOTORCOLD=hotstart)
-
-# Physical forcing
-
-TIDEFAC=on              # tide factor recalc
-   HINDCASTLENGTH=5.0      # length of initial hindcast, from cold (days)
-BACKGROUNDMET=GFS       # synoptic download/forcing
-   FORECASTCYCLE="06"
-TROPICALCYCLONE=off     # tropical cyclone forcing
-   STORM=05             # storm number, e.g. 05=ernesto in 2006
-   YEAR=2022            # year of the storm
-   VORTEXMODEL=GAHM     # default is GAHM (NWS20); ASYMMETRIC (NWS19) possible
-WAVES=on                # wave forcing
-   REINITIALIZESWAN=no  # used to bounce the wave solution
-   SWANHSFULL=no        # don't create a fulldomain swan hotstart file
-VARFLUX=off             # variable river flux forcing
-
-# Computational Resources
-
-NCPU=2                      # number of compute CPUs for all simulations
-NUMWRITERS=1
-NCPUCAPACITY=20
+TIDEFAC=on            # tide factor recalc
+HINDCASTLENGTH=30.0   # length of initial hindcast, from cold (days)
+BACKGROUNDMET=on      # NAM download/forcing
+FORECASTCYCLE="06"
+TROPICALCYCLONE=off   # tropical cyclone forcing
+STORM=09              # storm number, e.g. 05=ernesto in 2006
+YEAR=2021             # year of the storm
+WAVES=on             # wave forcing
+REINITIALIZESWAN=no   # used to bounce the wave solution
+VARFLUX=off           # variable river flux forcing
 CYCLETIMELIMIT="99:00:00"
+
+# Computational Resources (related defaults set in platforms.sh)
+
+NCPU=15                     # number of compute CPUs for all simulations
+NUMWRITERS=1
+NCPUCAPACITY=9999
 
 # Post processing and publication
 
-INTENDEDAUDIENCE=developers-only
+INTENDEDAUDIENCE=developers-only    # can also be "developers-only" or "professional"
 OPENDAPPOST=opendap_post2.sh
-#POSTPROCESS=( includeWind10m.sh createOPeNDAPFileList.sh $OPENDAPPOST )
-POSTPROCESS=( includeWind10m.sh  )
-OPENDAPNOTIFY="null"
+POSTPROCESS=( includeWind10m.sh createOPeNDAPFileList.sh )
+OPENDAPNOTIFY="coastalrisk.live@outlook.com,pub.coastalrisk.live@outlook.com,jason.fleming@seahorsecoastal.com,jason.fleming@stormsurge.live"
 #hooksScripts[FINISH_SPINUP_SCENARIO]=" output/createOPeNDAPFileList.sh output/$OPENDAPPOST "
 #hooksScripts[FINISH_NOWCAST_SCENARIO]=" output/createOPeNDAPFileList.sh output/$OPENDAPPOST "
 
 # Monitoring
 
-OPENDAPNOTIFY="null"
-NOTIFY_SCRIPT=null_notify.sh
 enablePostStatus="no"
 enableStatusNotify="no"
 statusNotify="null"
 
+# Initial state (overridden by STATEFILE after ASGS gets going)
+
+COLDSTARTDATE=2023071500
+HOTORCOLD=coldstart      # "hotstart" or "coldstart"
+LASTSUBDIR=null
+
 # Scenario package
 
 #PERCENT=default
-SCENARIOPACKAGESIZE=2 # number of storms in the ensemble
+SCENARIOPACKAGESIZE=2 # <====<<!!TWO TOTAL!! # number of scenarios
 case $si in
--2)
+ -2)
    ENSTORM=hindcast
    OPENDAPNOTIFY="null"
    ;;
@@ -94,12 +99,12 @@ case $si in
    ENSTORM=nowcast
    OPENDAPNOTIFY="null"
    ;;
- 0)
-   ENSTORM=gfsforecastWind10m
+0)
+   ENSTORM=namforecastWind10m
    source $SCRIPTDIR/config/io_defaults.sh # sets met-only mode based on "Wind10m" suffix
    ;;
 1)
-   ENSTORM=gfsforecast
+   ENSTORM=namforecast
    ;;
 *)
    echo "CONFIGRATION ERROR: Unknown scenario number: '$si'."
