5bd39d8392e9df2f9f6996be0aff94a17087294a
wwlwpd <46434714+wwlwpd@users.noreply.github.com>
2024-09-11 03:07:44 -0500

    updated config file

--- 2024/asgs_config_HSOFS_al062024_stampede3_be.sh
+++ 2024/asgs_config_HSOFS_al062024_stampede3_be.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
-#-- created on 2024-09-11 02:07:16 UTC, https://tools.adcirc.live --#
+#-- created on 2024-09-11 07:35:30 UTC, https://tools.adcirc.live --#
 
 # Copyright(C) 2024 Jason Fleming <jason.fleming@adcirc.live>
 # Copyright(C) 2024 Brett Estrade <brett.estrade@adcirc.live>
 
-# + additional Copyright and License info is at the bottom
+# All Copyright information must be retained when sharing, modifying,
+# or deriving works from this source code file; additional Copyright
+# and Licensing information should be at the bottom of this file.
 
 #-------------------------------------------------------------------
 # Instance and Operator Information
@@ -12,24 +14,33 @@
 #
 
 INSTANCENAME=HSOFS_al062024_stampede3_be
-# "name" of this ASGS process
-
+   # !! "name" of this ASGS process
 ASGSADMIN=asgsnotify@memenesia.net
-# email address of operator, HPCs need it
-
+   # !! email address of operator, HPCs need it
 ACCOUNT=TG-DMS080016N
-# used on HPC's to specify allocation account
-
-QOS=vipPJ_P3000
-# used for priority access at TACC
+   # !! used on HPC's to specify allocation account
+#QOS=vipPJ_P3000 # !!! causing submission on Stampede3 to fail
+   # !! used for priority access at TACC
 
 #-------------------------------------------------------------------
-# Input Files and Templates 
+# Grid and Domain Settings 
 #-------------------------------------------------------------------
 #
 
 GRIDNAME=HSOFS
+   # !! the "mesh"
 source $SCRIPTDIR/config/mesh_defaults.sh
+   # !! contains mesh defaults
+
+#-------------------------------------------------------------------
+# Logging Settings 
+#-------------------------------------------------------------------
+#
+
+enablePostStatus=yes
+enableStatusNotify=no
+statusNotify="asgsnotify@memenesia.net"
+   # !! required for JSON Logging
 
 #-------------------------------------------------------------------
 # Start State Information
@@ -37,79 +48,62 @@ source $SCRIPTDIR/config/mesh_defaults.sh
 #
 
 HOTORCOLD=hotstart
-# Note: Initial state (overridden by STATEFILE after ASGS gets going since
-# it's then a "hotstart")
-
-HINDCASTLENGTH=30 
-# length of initial hindcast, from cold (days)
-
+   # !! initial state (overridden by STATEFILE after ASGS gets going since it's then a "hotstart")
 COLDSTARTDATE=auto
-# ensures that COLDSTARTDATE is ignored, and it is gotten from the hotstart file
-
-LASTSUBDIR=https://fortytwo.cct.lsu.edu/thredds/fileServer/2024/nam/2024062806/HSOFS/qbd.loni.org/HSOFS_nam_be/namforecast      
-# used when HOTORCOLD=hotstart
+   # !! ensures that COLDSTARTDATE is ignored, and it is gotten from the hotstart file
+LASTSUBDIR=https://fortytwo.cct.lsu.edu/thredds/fileServer/2024/al06/09/HSOFS/mike.hpc.lsu.edu/HSOFS_al062024_mike_sn/nhcConsensus
+   # !! used when HOTORCOLD=hotstart
+HINDCASTLENGTH=30
+   # !! length of initial hindcast, from cold (days)
 
 #-------------------------------------------------------------------
 # Physical Forcing (defaults set in config/forcing_defaults.sh)
 #-------------------------------------------------------------------
 #
 
-TIDEFAC=on                       
-# tide factor recalc
+# Meteorological (winds - NAM, GFS, etc)
+BACKGROUNDMET=off
+   # !! download/ meteorological forcing from an upstream source
+###FORECASTCYCLE=""
+   # !! !! used when BACKGROUNDMET is turned on (e.g., "00,06,12,18"), in UTC / "Z"
 
-BACKGROUNDMET=off           
-# download/ meteorological forcing from an upstream source
+# Tropical/Hurricane (ATCF data for internal GAHM wind generation)
+TROPICALCYCLONE=on
+   # !! tropical cyclone forcing (mutually exclusive with BACKGROUNDMET in most cases)
+   STORM=06
+   # !! !! storm number, e.g. 05=ernesto in 2006
+   YEAR=2024
+   # !! !! year of the storm
+   BASIN=al
+   # !! !! ocean basin, e.g., AL (Atlantic), EP (East Pacific), CP (Central Pacific)
 
-FORECASTCYCLE=""
-# used when BACKGROUNDMET is turned on ("on", "NAM", "GFS", etc)
-
-TROPICALCYCLONE=on       
-# tropical cyclone forcing (mutually exclusive with BACKGROUNDMET in most cases)
-
-   STORM=06                        
-   # storm number, e.g. 05=ernesto in 2006
-
-   YEAR=2024                          
-   # year of the storm
-
-   BASIN=al                        
-   # ocean basin, e.g., AL (Atlantic), EP (East Pacific)
-WAVES=on                           
-# wave forcing via built-in SWAN coupling (adcswan/padcswan)
-
-   REINITIALIZESWAN=off  
-   # used to bounce the wave solution (only used when WAVES=on)
-
-VARFLUX=off                       
-# variable river flux forcing
-
-CYCLETIMELIMIT=99:00:00         
-# max time, usually just 99:00:00
+# Other
+TIDEFAC=on
+   # !! tide factor recalc
+WAVES=on
+   # !! wave forcing via built-in SWAN coupling (adcswan/padcswan)
+   REINITIALIZESWAN=off
+   # !! !! used to bounce the wave solution (only used when WAVES=on)
+VARFLUX=off
+   # !! variable river flux forcing
+CYCLETIMELIMIT=99:00:00
+   # !! max time, usually just 99:00:00
 
 #-------------------------------------------------------------------
 # Computational Resources (related defaults set in ./platforms[.sh])
 #-------------------------------------------------------------------
 #
 
-PPN=48                  
-# platform specific, processors-per-node
-
-NCPU=959                
-# number of compute CPUs for all simulations, should be a set in consideration of PPN
-
-NUMWRITERS=1    
-# usually just 1, total CPUs for the run is NCPU+NUMWRITERS
-
-QUEUESYS=SLURM  
-# platform specific, e.g., SLURM
-
+QUEUESYS=SLURM
+   # !! platform specific, e.g., SLURM
+PPN=48
+   # !! platform specific, processors-per-node
+NCPU=959
+   # !! number of compute CPUs for all simulations, should be a set in consideration of PPN
+NUMWRITERS=1
+   # !! usually just 1, total CPUs for the run is NCPU+NUMWRITERS
 NCPUCAPACITY=9999
-# max number of total CPUs to use
-
-enablePostStatus=yes
-enableStatusNotify=no
-statusNotify="asgsnotify@memenesia.net"
-# JSON Logging
+   # !! max number of total CPUs to use
 
 #-------------------------------------------------------------------
 # Post processing and publication
@@ -117,27 +111,23 @@ statusNotify="asgsnotify@memenesia.net"
 #
 
 EMAILNOTIFY=yes
-
-INTENDEDAUDIENCE=general             
-# Settings for CERA to help determine where to post results; "general" | "developers-only"
-
-OPENDAPPOST=opendap_post2.sh                       
-# usually, "opendap_post2.sh"; for posting to OpenDAP/THREDDS servers via ssh
-
-POSTPROCESS=(  $OPENDAPPOST ) 
-# scripts to run during the POSTPROCESS ASGS hook
-# email list receiving alerts via bin/asgs-sendmail
-
-OPENDAPNOTIFY="coastalrisk.live@outlook.com,pub.coastalrisk.live@outlook.com,asgs.cera.lsu@coastalrisk.live,asgs.cera.pub.lsu@coastalrisk.live,asgsnotify@memenesia.net,jasongfleming@gmail.com,cdelcastillo21@gmail.com"
-
-NOTIFY_SCRIPT=cera_notify.sh                   
-# notification used ... 
-
-TDS=( lsu_tds )                                   
-# servers receiving results via ssh
+   # !! email notification master switch
+INTENDEDAUDIENCE=general
+   # !! used by CERA to pick where to display result; "general" | "developers-only"
+OPENDAPPOST=opendap_post2.sh
+   # !! posts OpenDAP/THREDDS servers via ssh (default, opendap_post2.sh)
+POSTPROCESS=(  $OPENDAPPOST )
+   # !! scripts to run during the POSTPROCESS ASGS hook
+OPENDAPNOTIFY="coastalrisk.live@outlook.com,pub.coastalrisk.live@outlook.com,asgs.cera.lsu@coastalrisk.live,asgs.cera.pub.lsu@coastalrisk.live,asgsnotify@memenesia.net,jasongfleming@gmail.com"
+   # !! main set of email addresses to notify
+NOTIFY_SCRIPT=cera_notify.sh
+   # !! notification used ...
+TDS=( tacc_tds3 lsu_tds )
+   # !! servers receiving results via ssh
 
 hooksScripts[FINISH_SPINUP_SCENARIO]=" output/createOPeNDAPFileList.sh output/$OPENDAPPOST "
 hooksScripts[FINISH_NOWCAST_SCENARIO]=" output/createOPeNDAPFileList.sh output/$OPENDAPPOST "
+   # !! additional hook scripts, required for correctly posting data to CERA
 
 #-------------------------------------------------------------------
 # Scenario Package (Ensemble) Settings
@@ -146,25 +136,30 @@ hooksScripts[FINISH_NOWCAST_SCENARIO]=" output/createOPeNDAPFileList.sh output/$
 
 # Used for Hindcast Only configurations
 #HINDCASTONCE_AND_EXIT=
+   # !! if set, will cause asgs_main.sh (main loop) to exit after the first hindcast
 #PERCENT=default
-
-SCENARIOPACKAGESIZE=6 
-
+   # !! default is the track as described by the ATCF data; veerRight is positive;
+   # !! veerLeft is negative. 100 is wrt the right most edge of the cone, -100 is
+   # !! wrt left most edge of the cone
+SCENARIOPACKAGESIZE=4
+   # !! GAHM (using ATCF/BEST data) can have many different scenarios
+   # !! as the tracks of the storm may be altered; here there are 4
+   # !! scenarios, not including the hindcast and the nowcast
 case $si in
  -2)
-   ENSTORM=hindcast           
+   ENSTORM=hindcast
    # initial ramp up during a coldstart
-   OPENDAPNOTIFY="coastalrisk.live@outlook.com,pub.coastalrisk.live@outlook.com,asgs.cera.lsu@coastalrisk.live,asgs.cera.pub.lsu@coastalrisk.live,asgsnotify@memenesia.net,jasongfleming@gmail.com,cdelcastillo21@gmail.com"
+   OPENDAPNOTIFY="coastalrisk.live@outlook.com,pub.coastalrisk.live@outlook.com,asgs.cera.lsu@coastalrisk.live,asgs.cera.pub.lsu@coastalrisk.live,asgsnotify@memenesia.net,jasongfleming@gmail.com"
    ;;
 -1)
-   ENSTORM=nowcast            
-   # do nothing ... this is "catch up", not a forecast 
+   ENSTORM=nowcast
+   # do nothing ... this is "catch up", not a forecast
    OPENDAPNOTIFY="null"
    ;;
 0)
    ENSTORM=nhcConsensus
    PERCENT=0
-   OPENDAPNOTIFY="coastalrisk.live@outlook.com,pub.coastalrisk.live@outlook.com,asgs.cera.lsu@coastalrisk.live,asgs.cera.pub.lsu@coastalrisk.live,asgsnotify@memenesia.net,jasongfleming@gmail.com,cdelcastillo21@gmail.com"
+   OPENDAPNOTIFY="coastalrisk.live@outlook.com,pub.coastalrisk.live@outlook.com,asgs.cera.lsu@coastalrisk.live,asgs.cera.pub.lsu@coastalrisk.live,asgsnotify@memenesia.net,jasongfleming@gmail.com"
    source $SCRIPTDIR/config/io_defaults.sh
    ;;
 1)
@@ -176,24 +171,12 @@ case $si in
 2)
    ENSTORM=veerRight100
    PERCENT=100
-   OPENDAPNOTIFY="coastalrisk.live@outlook.com,pub.coastalrisk.live@outlook.com,asgs.cera.lsu@coastalrisk.live,asgs.cera.pub.lsu@coastalrisk.live,asgsnotify@memenesia.net,jasongfleming@gmail.com,cdelcastillo21@gmail.com"
+   OPENDAPNOTIFY="coastalrisk.live@outlook.com,pub.coastalrisk.live@outlook.com,asgs.cera.lsu@coastalrisk.live,asgs.cera.pub.lsu@coastalrisk.live,asgsnotify@memenesia.net,jasongfleming@gmail.com"
    source $SCRIPTDIR/config/io_defaults.sh
    ;;
 3)
    ENSTORM=veerRight100Wind10m
    PERCENT=100
-   OPENDAPNOTIFY="asgsnotify@memenesia.net"
-   source $SCRIPTDIR/config/io_defaults.sh
-   ;;
-4)
-   ENSTORM=veerLeft100
-   PERCENT=-100
-   OPENDAPNOTIFY="coastalrisk.live@outlook.com,pub.coastalrisk.live@outlook.com,asgs.cera.lsu@coastalrisk.live,asgs.cera.pub.lsu@coastalrisk.live,asgsnotify@memenesia.net,jasongfleming@gmail.com,cdelcastillo21@gmail.com"
-   source $SCRIPTDIR/config/io_defaults.sh
-   ;;
-5)
-   ENSTORM=veerLeft100Wind10m
-   PERCENT=-100
    OPENDAPNOTIFY="asgsnotify@memenesia.net"
    source $SCRIPTDIR/config/io_defaults.sh
    ;;
@@ -245,5 +228,4 @@ HINDCASTARCHIVE=prepped_${GRIDNAME}_hc_${INSTANCENAME}_${NCPU}.tar.gz
 # the ASGS.  If not, see <http://www.gnu.org/licenses/>.
 #-------------------------------------------------------------------
 
-#-- created on 2024-09-11 02:07:16 UTC, https://tools.adcirc.live --#
-
+#-- created on 2024-09-11 07:35:30 UTC, https://tools.adcirc.live --#
